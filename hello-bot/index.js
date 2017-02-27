7531cadb370e1a63bfb54a2469f24a32df2a74fa
Arnaud Langelier <arnaud.langelier@etu.univ-nantes.fr>
2017-02-27 16:02:05 +0100

    testing the understanding feature of wit.ai module

--- hello-bot/index.js
+++ hello-bot/index.js
@@ -1,19 +1,29 @@
 module.exports = function(bp) {
-  bp.middlewares.load();
+    bp.middlewares.load();
 
-  const payload = {
-  	template_type: "button",
-  	text: "Tu veux participer à la prochaine na c'est ça ?",
-  	buttons: [
+    const naButton = {
+  	 template_type: "button",
+  	 text: "Tu veux participer à la prochaine na c'est ça ?",
+  	 buttons: [
   		{
   			type: "web_url",
   			url: "http://www.nantarena.net",
   			title: "Go t'inscrire !"
   		}
-  	]
-  }
+  	 ]
+    }
 
-  bp.hear(/nantarena/i, (event, next) => {
-    bp.messenger.sendTemplate(event.user.id, payload, {typing:true});
-  });
+    bp.hear(/nantarena/i, (event, next) => {
+        bp.messenger.sendTemplate(event.user.id, naButton, {typing:true});
+    });
+
+    bp.hear({'wit.entities.intent[0].value': 'prix'}, (event, next) => {
+        console.log('>> Prix');
+        bp.messenger.sendText(event.user.id, "Tu me parles d'argent.");
+    })
+
+    bp.hear({'wit.entities.intent[0].value': 'Hitler'}, (event, next) => {
+        console.log('>> SVP pas la LICRA');
+        bp.messenger.sendText(event.user.id, "Allons allons, est-on obligé de parler de ça ?");
+    })
 }
